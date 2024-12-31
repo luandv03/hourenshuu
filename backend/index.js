f24b7e704257aed0f5e7e49620457ae9bbddb539
Dinh Van Luan <dinhvanluan2k3@gmail.com>
2024-12-31 11:07:31 +0700

    fix cors upload file

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL,
-        methods: ["GET", "POST", "DELETE", "PUT"],
-        allowedHeaders: ["Content-Type", "Authorization", "token"],
-    })
-);
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Server is working");
